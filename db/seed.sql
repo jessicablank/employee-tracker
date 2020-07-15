2999ab2574ed9cc799c8622aab580b4eb799e16f
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-15 10:03:18 -0700

    reorganizing sql files into db folder

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+-- Departments as of 14 July 2020 --
+INSERT INTO departments (dept_id, department_name)
+VALUES 
+(1,"Operations"),
+(2,"Engineering & Research"),
+(3,"Security"),
+(4,"Sales"),
+(5,"Public Relations");
+
