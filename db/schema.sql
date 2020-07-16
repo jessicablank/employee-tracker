831ed7f05c60d90f3cf7ac50a9e6077f25bd3289
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-15 19:57:33 -0700

    Removed sample data

--- db/schema.sql
+++ db/schema.sql
@@ -23,9 +23,10 @@ CREATE TABLE employee_data(
   firstName VARCHAR(30),
   lastName VARCHAR(30),
   roleID INT NOT NULL,
-  managerID INT,
+
   PRIMARY KEY (id),
   FOREIGN KEY (roleID) REFERENCES roles_emp(id)
 );
 
+--  managerID INT,
 --FOREIGN KEY (managerID) REFERENCES roles_emp(id)
