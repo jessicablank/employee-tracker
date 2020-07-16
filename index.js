42ffb2cfca68b8b0d8b0303cf63c6ad0fd4a2e6a
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-15 20:57:35 -0700

    Removing sample data

---
+++ index.js
@@ -0,0 +1,27 @@
+// import dependencies (mysql, inquirer, console.table)
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+// create a mysql connection
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "bones",
+    database: "employee_db",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("Unable to connect to data source. Good bye.");
+    } else {
+      mainMenu();
+    }
+  });
